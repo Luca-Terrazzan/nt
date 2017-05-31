beb93e9b6ace8a17870da9c94909df5672a43031
Luca Terrazzan <luca.terrazzan@telnext.com>
2017-05-31 11:14:11 +0200

    Updated .sql files
    tables.sql = tables creation, data.sql = data insert

---
+++ tables.sql
@@ -0,0 +1,14 @@
+-- TODO: add indexes
+CREATE TABLE node_tree(
+    idNode SMALLINT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    level SMALLINT UNSIGNED NOT NULL,
+    iLeft SMALLINT UNSIGNED NOT NULL UNIQUE KEY,
+    iRight SMALLINT UNSIGNED NOT NULL UNIQUE KEY
+) ENGINE=InnoDb;
+
+CREATE TABLE node_tree_names(
+    idNode SMALLINT UNSIGNED NOT NULL,
+    language ENUM('italian', 'english'),
+    nodeName VARCHAR(255) NOT NULL,
+    FOREIGN KEY (idNode) REFERENCES node_tree(idNode)
+) ENGINE=InnoDb;
