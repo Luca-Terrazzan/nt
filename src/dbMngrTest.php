3eede824e45702b3fe3c7af1820ff1a7a5eaeb56
Luca Terrazzan <luca.terrazzan@telnext.com>
2017-06-01 00:02:42 +0200

    updated tests

---
+++ src/dbMngrTest.php
@@ -0,0 +1,35 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use Assignment\Core\DbMngr;
+
+require_once __DIR__ . '/dbMngr.php';
+
+final class NestedSetHandlerTest extends TestCase
+{
+    public function testConstructor()
+    {
+        $dbmngr = DbMngr::getInstance();
+        $this->assertInstanceOf(DbMngr::class, $dbmngr);
+    }
+
+    /**
+     * Test queries.
+     * WARNING: this test is heavily dependant on actual data, not a proper unit test!
+     */
+    public function testQuery()
+    {
+        $dbmngr = DbMngr::getInstance();
+        $queryResult = $dbmngr->executeQuery('SELECT * FROM node_tree');
+        $this->assertEquals(count($queryResult), 12);
+    }
+
+    public function testInjectionSafety()
+    {
+        $dbmngr = DbMngr::getInstance();
+        $queryResult = $dbmngr->executeQuery('SELECT * FROM node_tree; SELECT * FROM node_tree_names');
+        $this->assertEquals($queryResult, 'Uh oh...Something\'s wrong with your query...Are you trying to inject?!?');
+        $queryResult = $dbmngr->executeQuery('SELECT * FROM \'node_tree;');
+        $this->assertEquals($queryResult, 'Uh oh...Something\'s wrong with your query...Are you trying to inject?!?');
+    }
+}
