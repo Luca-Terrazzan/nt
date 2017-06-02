30a6981eb70e11ed3d5084a55761f93b82708a2d
Luca Terrazzan <luca.terrazzan@telnext.com>
2017-06-02 11:38:01 +0200

    new queries

--- queries.sql
+++ queries.sql
@@ -1,8 +1,9 @@
 SELECT * FROM node_tree;
 SELECT * FROM node_tree_names;
 
+-- direct children
 SELECT Child.idNode, Child.iLeft, Child.iRight, Child.level
-FROM node_tree as Child, node_tree as Parent
+FROM node_tree AS Child, node_tree AS Parent
 WHERE
 	Child.level = Parent.level + 1
 	AND Child.iLeft > Parent.iLeft
@@ -10,3 +11,22 @@ WHERE
     AND Parent.level = 2
 	AND Parent.iLeft = 12
     AND Parent.iRight = 19
+
+--  all children
+SELECT Child.idNode, Child.iLeft, Child.iRight, Child.level
+FROM node_tree AS Child, node_tree AS Parent
+WHERE
+	Child.iLeft > Parent.iLeft
+	AND Child.iRight < Parent.iRight
+	AND Parent.iLeft = 12
+    AND Parent.iRight = 19
+
+SELECT Child.idNode, Trans.nodeName
+FROM node_tree AS Child, node_tree AS Parent, node_tree_names AS Trans
+WHERE
+	Child.iLeft > Parent.iLeft
+	AND Child.iRight < Parent.iRight
+	AND Parent.iLeft = 12
+    AND Parent.iRight = 19
+    AND Trans.idNode = Child.idNode
+    AND Trans.language = 'Italian';
